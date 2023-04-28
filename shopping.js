c7e41565d2fa73b602aa04fa7ead6a1c021f9863
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-28 18:43:52 +0600

    table add

--- shopping.js
+++ shopping.js
@@ -1,3 +1,18 @@
+const cardArray = [];
+
+function dislay (cardProduct){
+    // console.log(cardProduct);
+    const tableBody = document.getElementById('card-products');
+    for(let i=0; i<cardProduct.length; i++){
+        // console.log(cardArray[i]);
+        const name = cardArray[i].productName;
+        const price = cardArray[i].productPrice;
+
+        const tr = document.createElement('tr');
+        
+    }
+}
+
 function addToCard (element){
     // console.log('button clicked');
     // console.log(element.parentNode.parentNode.children[0].innerText);
@@ -11,5 +26,11 @@ function addToCard (element){
         productName:productName,
         productPrice:parseInt(productPrice)
     }
-    console.log(productObj);
+    // console.log(productObj);
+    cardArray.push(productObj);
+    // console.log(cardArray);
+    // console.log(cardArray,length);
+
+    document.getElementById('total-added-product').innerText = cardArray.length;
+    dislay(cardArray);
 }
