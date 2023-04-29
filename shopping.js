6f89a639c6b231a6f8e99a495ff8203519d28aec
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-29 23:02:27 +0600

    project complete and total price added

--- shopping.js
+++ shopping.js
@@ -1,6 +1,7 @@
 const cardArray = [];
 
 function display(cardProduct){
+    let totalPrice = 0;
     // console.log(cardProduct);
     const tableBody = document.getElementById("card-products");
     tableBody.innerHTML = "";
@@ -10,18 +11,25 @@ function display(cardProduct){
         const name = cardArray[i].productName;
         const price = cardArray[i].productPrice;
 
+        totalPrice = totalPrice + price;
+
         const tr = document.createElement("tr");
         tr.innerHTML = `
         <th> ${i + 1} </th>
         <td> ${name} </td>
         <td> ${price} </td>
         `;
-        if(tableBody){
+       
             tableBody.appendChild(tr);
-           
-        }
-        // console.log(tr);
+           // console.log(tr);
     }
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td> Total Price </td>
+    <td> ${totalPrice}</td>
+    `;
+    tableBody.appendChild(tr);
 }
 
 function addToCard (element){
